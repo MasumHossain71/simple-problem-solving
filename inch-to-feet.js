8daebb176ffa1f4089f2a402a368d5aedf3d7acb
MasumHossain71 <masumhazi311@gmail.com>
2022-11-28 01:37:23 +0600

    this is first diploy in myb code

---
+++ inch-to-feet.js
@@ -0,0 +1,9 @@
+function inchToFeet(inch){
+   var feet = inch/12;
+   return feet;
+}
+
+var nanaFeet = inchToFeet(1268);
+console.log(nanaFeet);
+var naniFeet = inchToFeet(37);
+console.log(naniFeet);
