98d5d9d717d0e36926760d32516a1fc675cc5943
MasumHossain71 <masumhazi311@gmail.com>
2022-11-28 23:01:07 +0600

    added a new concept on this program

--- leapYear.js
+++ leapYear.js
@@ -1,10 +1,18 @@
-function yCalculation(leapYear) {
-  const reminder = leapYear % 4;
-  if (reminder == 0) {
-    console.log("Your year is a leap year.");
-  } else {
-    console.log("Your year is not a leap year.");
-  }
+function leapYear(years){
+   var result; 
+   if (years/400){
+     result = true
+   }
+   else if(years/100){
+     result = false
+   }
+   else if(years/4){
+     result= true
+   }
+   else{
+     result= false
+   }
+   return result
 }
-yCalculation(2022);
-yCalculation(2024)
+let calculation = leapYear(2024);
+console.log(calculation);
