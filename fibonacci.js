bf0ad79eb198ff4de0c65e63eb92c145cd3993a1
MasumHossain71 <masumhazi311@gmail.com>
2022-11-29 17:22:30 +0600

    fibonacci calculatuion in recursive way

--- fibonacci.js
+++ fibonacci.js
@@ -1,9 +1,25 @@
+// function fibonacci(n){
+//    let fibo = [0,1];
+//    for (var i = 2; i<= n; i++){
+//       fibo[i] = fibo[i - 1] + fibo[i - 2];
+//    }
+//    return fibo;
+// }
+// let result = fibonacci(20);
+// console.log(result);
+
+// fibonacci calculation in recursive way 
+
 function fibonacci(n){
-   let fibo = [0,1];
-   for (var i = 2; i<= n; i++){
-      fibo[i] = fibo[i - 1] + fibo[i - 2];
+   if(n == 0){
+      return 0;
    }
-   return fibo;
+   else if(n == 1){
+      return 1;
+   }
+   else{
+      return fibonacci(n-1) + fibonacci(n-2);
+   }
 }
-let result = fibonacci(20);
+let result = fibonacci(30);
 console.log(result);
