25df905117807bc3d8f5e242bcc5e3488a52c224
MasumHossain71 <masumhazi311@gmail.com>
2022-11-29 11:13:46 +0600

    factorial reduced using function

---
+++ factorial.js
@@ -0,0 +1,19 @@
+function factorial(n){
+   let factorial = 1;
+   for (var i = 1; i<= n; i++){
+      factorial =  factorial * i
+   }
+   return factorial;
+}
+let result = factorial(20);
+console.log(result);
+
+function factCal(number){
+   let factorial = 1;
+   for (var i = 1; i <= n; i++){
+      factorial = factorial * i;
+   }
+   return factorial;
+}
+let factSum = factorial(10);
+console.log(factSum);
